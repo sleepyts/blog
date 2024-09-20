97bcf2801fe31bd7c750df67ecd89e7d53bd2f27
sleepyts <choujack373@gamil.com>
2024-09-20 19:14:45 +0800

    commit

---
+++ blog-server/src/main/java/com/ts/Mapper/SensitiveWordMapper.java
@@ -0,0 +1,15 @@
+package com.ts.algorithm.sensitiveWord.sensitive.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ts.algorithm.sensitiveWord.sensitive.domain.SensitiveWord;
+
+/**
+ * 敏感词Mapper
+ *
+ * @author zhaoyuhang
+ * @since 2023-05-21
+ */
+public interface SensitiveWordMapper extends BaseMapper<SensitiveWord> {
+
+}
