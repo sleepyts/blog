0abbd52ad54e621efbbfb2606a99e1c76bf47149
sleepyts <choujack373@gamil.com>
2024-10-10 20:03:17 +0800

    commit

--- blog-server/src/main/java/com/ts/Controller/GithubWebHookController.java
+++ blog-server/src/main/java/com/ts/Controller/GithubWebHookController.java
@@ -2,21 +2,51 @@ package com.ts.Controller;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
 
+import am.ik.webhook.HmacWebhookSigner;
+import am.ik.webhook.WebhookHttpHeaders;
+import am.ik.webhook.WebhookSigner;
+import am.ik.webhook.WebhookVerifier;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.codec.binary.Hex;
 import org.checkerframework.common.reflection.qual.GetClass;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
 
 @RestController
 @Slf4j
 public class GithubWebHookController {
 
+    @Value("${config.github.secret}")
+    private static String gitHubSecret;
+
     @PostMapping("/webhook")
-    public String receiveGithubPush() {
+    public ResponseEntity<String> receiveGithubPush(@RequestBody String payload,
+                                                    @RequestHeader("X-Hub-Signature-256") String signature) {
         try {
-            // 拉取最新代码
+            try{
+                verify(signature, payload);
+            }catch (Exception e){
+                log.info("Invalid signature.");
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid signature");
+            }
+            log.info("Signature verified successfully.");
+
+            // 执行 git pull
             ProcessBuilder gitPull = new ProcessBuilder("git", "pull");
             gitPull.directory(new java.io.File("/root/java/blog")); // 指定仓库路径
             Process process = gitPull.start();
@@ -30,14 +60,19 @@ public class GithubWebHookController {
             process.waitFor();
 
             // 执行自定义命令
-            ProcessBuilder customCommand = new ProcessBuilder("echo ok");
+            ProcessBuilder customCommand = new ProcessBuilder("echo", "ok");
             Process customProcess = customCommand.start();
             customProcess.waitFor();
 
         } catch (Exception e) {
-            e.printStackTrace();
-            return "Failed to process webhook: " + e.getMessage();
+            log.error("Failed to process webhook", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to process webhook: " + e.getMessage());
         }
-        return "Webhook received and processed";
+        return ResponseEntity.ok("Webhook received and processed");
+    }
+    private static void verify(String signature, String body) {
+        HmacWebhookSigner webhookSigner = new HmacWebhookSigner("SHA256", gitHubSecret);
+        WebhookVerifier verifier = new WebhookVerifier(webhookSigner, WebhookSigner.Encoder.HEX);
+        verifier.verify(body, signature);
     }
 }
--- blog-server/src/test/java/com/ts/test1111ApplicationTest.java
+++ blog-server/src/test/java/com/ts/test1111ApplicationTest.java
@@ -1,30 +1,30 @@
-package com.ts;
-
-import java.awt.List;
-import java.lang.reflect.Field;
-
-import com.ts.Config.RabbitMQConfig;
-import org.apache.naming.factory.BeanFactory;
-import org.junit.jupiter.api.Test;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.data.redis.core.RedisTemplate;
-
-@SpringBootTest
-class test1111ApplicationTest {
-
-    @Test
-    void contextLoads() throws InstantiationException, IllegalAccessException {
-        Integer a = 2;
-        Field[] fields = a.getClass().getFields();
-
-        for (Field field : fields) {
-            System.out.println(field.getName());
-        }
-    }
-
-    @Autowired
-    private RabbitTemplate myRabbitTemplate;
-}
+//package com.ts;
+//
+//import java.awt.List;
+//import java.lang.reflect.Field;
+//
+//import com.ts.Config.RabbitMQConfig;
+//import org.apache.naming.factory.BeanFactory;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.data.redis.core.RedisTemplate;
+//
+//@SpringBootTest
+//class test1111ApplicationTest {
+//
+//    @Test
+//    void contextLoads() throws InstantiationException, IllegalAccessException {
+//        Integer a = 2;
+//        Field[] fields = a.getClass().getFields();
+//
+//        for (Field field : fields) {
+//            System.out.println(field.getName());
+//        }
+//    }
+//
+//    @Autowired
+//    private RabbitTemplate myRabbitTemplate;
+//}
